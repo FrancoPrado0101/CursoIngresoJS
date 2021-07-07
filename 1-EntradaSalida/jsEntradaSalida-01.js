c1dcc8374ce197216b23c4474e2744d953680fd6
FrancoPrado0101 <francoprado2021@gmail.com>
2021-07-06 21:05:18 -0300

    Actualización

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola otra vez");
+	alert("Hey, otra vez tú");
 }
 
